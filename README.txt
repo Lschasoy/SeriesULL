fe490814f6f100e0732c5f87fd1e1fa09af24f8f
Leonardo <alu3270@etsii.ull.es>
2012-10-08 10:05:29 +0100

    Creacion de la App

--- README.txt
+++ README.txt
@@ -6,5 +6,9 @@
 #
 #
 # Colaboradores: 
-      Alberto, Adrian, Alexandra, Ivan, Hamilton, Leo, Marcos
+#      Alberto, Adrian, Alexandra, Ivan, Hamilton, Leo, Marcos
 #=====================================================
+# Software Instalado en el Centro de Calculo.
+#
+#!  ruby 1.9.3p0 (2011-10-30 revision 33570) [i686-linux]
+#!  Rails 3.0.0   
---
+++ Serie/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Serie::Application.config.secret_token = 'cbefdd87dd5664176d6c9397f9e51fa69072b733eff0eff534b547bf68d56c060d97f00cf25a2ebda38f4905f71ea2f0b7e55860613a58cf8a1103e0f99876d4'
