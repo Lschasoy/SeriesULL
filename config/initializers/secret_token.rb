c927924c61f8591cbc9d2c548f6b675e1bbd0354
Alberto <alberto_00715@hotmail.com>
2012-10-26 16:19:06 +0100

    Nuevo proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeriesJump::Application.config.secret_token = '8af6e0996b79873847279ec42c185ca2376957db3b4b954e9eec898eb381bafd5ff5e5e056895a286c2d1e2bb2d91abd5c606a904c184eaa1fdea7e71c1f5c15'
